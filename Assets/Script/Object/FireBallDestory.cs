16c92fe7c9262bcf818e33b4c435a5374991b2f6
Carp <A110082014@mail.shu.edu.tw>
2023-09-13 21:04:53 +0800

    Fireball fixed

--- Assets/Script/Object/FireBallDestory.cs
+++ Assets/Script/Object/FireBallDestory.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBallDestroy : MonoBehaviour
-{  
-    private void Start() {
-        Destroy(gameObject , 5f);
+{
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
     }
-   private void OnCollisionEnter2D(Collision2D other) {
-    
-        if(other.gameObject.tag == "Explosive")
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Explosive")
         {
             Explosion explode = other.gameObject.GetComponent<Explosion>();
             explode.OnCollisionExplode();
         }
-        else if(other.gameObject.tag == "WoodPillar")
+        else if (other.gameObject.tag == "WoodPillar")
         {
             WoodPillar woodPillar = other.gameObject.GetComponent<WoodPillar>();
             woodPillar.Collapse();
         }
-        else if(other.gameObject.tag != "Player")
+        else if (other.gameObject.tag != "Player")
         {
             Destroy(gameObject);
         }
-        Destroy(gameObject);
+        //Destroy(gameObject);
         Debug.Log(other.gameObject.name);
     }
 
